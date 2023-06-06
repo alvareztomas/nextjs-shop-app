817a678edda8e939973494a85734218e3be00d77
Alvarez Tomas <alvareztomas.it@gmail.com>
2023-06-06 15:14:34 -0300

    feat: social media icons added

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,11 +1,41 @@
 import React from "react";
 import styles from "./footer.module.css";
+import Image from "next/image";
 
 const Footer = () => {
   return (
     <div className={styles.container}>
       <div>©2023 Annoyed. All rights reserved.</div>
-      <div></div>
+      <div className={styles.social}>
+        <Image
+          src="/fb_logo.png"
+          alt="Facebook account"
+          width={20}
+          height={20}
+          className={styles.icon}
+        />
+        <Image
+          src="/ig_logo.png"
+          alt="Instagram account"
+          width={20}
+          height={20}
+          className={styles.icon}
+        />
+        <Image
+          src="/tw_logo.png"
+          alt="Twitter account"
+          width={20}
+          height={20}
+          className={styles.icon}
+        />
+        <Image
+          src="/yt_logo.png"
+          alt="Youtube account"
+          width={20}
+          height={20}
+          className={styles.icon}
+        />
+      </div>
     </div>
   );
 };
