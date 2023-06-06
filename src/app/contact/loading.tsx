f5c87dc18ae725b997807bdbf18444ecb1998fa7
Alvarez Tomas <alvareztomas.it@gmail.com>
2023-06-06 17:12:19 -0300

    feat: contact page basic design

--- src/app/contact/loading.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Loading = () => {
-  return <div>Loading putito</div>;
-};
-
-export default Loading;
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,8 +1,34 @@
 import React from "react";
 import styles from "./page.module.css";
+import Image from "next/image";
+import Button from "@/components/Button/button";
 
 const Contact = () => {
-  return <div className={styles.container}>Contact</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Feel free to contact me!</h1>
+      <div className={styles.content}>
+        <div className={styles.imgContainer}>
+          <Image
+            src="/contact1.png"
+            alt="Illidan stormrage"
+            fill={true}
+            className={styles.img}
+          />
+        </div>
+        <form className={styles.form}>
+          <input type="text" placeholder="Name" className={styles.input} />
+          <input type="text" placeholder="Email" className={styles.input} />
+          <textarea
+            className={styles.textArea}
+            placeholder="Message"
+            cols={30}
+            rows={10}></textarea>
+          <Button url="" text="Submit" />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
