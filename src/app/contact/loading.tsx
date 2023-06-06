9c75ada00470e8a283647c1816f76366d9ca135d
Alvarez Tomas <alvareztomas.it@gmail.com>
2023-06-05 21:12:50 -0300

    chore: created base project structure

---
+++ src/app/contact/loading.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Loading = () => {
+  return <div>Loading putito</div>;
+};
+
+export default Loading;
---
+++ src/app/dashboard/settings/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./page.module.css";
+
+const Settings = () => {
+  return <div className={styles.container}>Settings</div>;
+};
+
+export default Settings;
